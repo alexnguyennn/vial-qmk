4f204c37aa4ae9001e35150af320b438d5728a5c
Alex Nguyen <alex.nguyen@proton.me>
2025-07-04 20:34:57 +1000

    test: got first mod custom working with tap hold

--- keyboards/svalboard/keymaps/alex/keymap.c
+++ keyboards/svalboard/keymaps/alex/keymap.c
@@ -17,35 +17,76 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "../keymap_support.c"
 #include "../keymap_support.h"
+#include "action_layer.h"
 #include "keycodes.h"
+#include "modifiers.h"
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 #include <stdbool.h>
 #include <stdint.h>
 #include "svalboard.h"
 #include "vial.h"
-#define RANGE_START SV_SAFE_RANGE
+// start from last custom qk keycode in keymap_support.h
+// SV_SAFE_RANGE is for safe keys only in qmk
+// keys we want to show in vial should be QK_KB_0 onwards
+#define RANGE_START SV_TOGGLE_AUTOMOUSE + 1
+
+// Define the keycode. The "KC_0" is an arbitrary placeholder that is never sent.
+#define HYPR_BSLSH ALL_T(KC_BACKSLASH)
+#define CTL_SFT LCTL_T(KC_0)
 
 enum custom_keycodes { SPACE_HYPR_L5 = RANGE_START };
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
         case SPACE_HYPR_L5:
+        case CTL_SFT:
             if (record->event.pressed) {
-                if (record->tap.count && !record->tap.interrupted) {
-                    // Tapped
-                    tap_code(KC_SPACE);
-                } else {
-                    // Held
-                    register_mods(MOD_HYPR);
-                    layer_on(5);
-                }
-            } else {
-                // Released
-                unregister_mods(MOD_HYPR);
-                layer_off(5);
+                // Send the string "hello" when the key is pressed
+                send_string("hello");
+                return false;
+                // if (record->tap.count && !record->tap.interrupted) {
+                //     // Tapped
+                //     tap_code(KC_SPACE);
+                // } else {
+                //     // Held
+                //     register_mods(MOD_HYPR);
+                //     layer_on(5);
+                // }
+                // } else {
+                //     // Released
+                //     unregister_mods(MOD_HYPR);
+                //     layer_off(5);
+                // }
+                // return false;
             }
-            return false;
+        // Implements:
+        // * on tap:         one-shift Shift
+        // * on hold:        Ctrl
+        // * on tap + hold:  Shift
+        case HYPR_BSLSH:
+            if (record->tap.count > 0) {
+                if (record->tap.count == 1) { // Single tap.
+                    if (record->event.pressed) {
+                        // add_oneshot_mods(MOD_BIT_LSHIFT);
+                        // send_string("hello");
+                        tap_code(KC_BACKSLASH);
+                    }
+                } else { // Tap + hold.
+                    if (record->event.pressed) {
+                        register_mods(MOD_HYPR);
+                        layer_on(4);
+                    } else {
+                        // unregister_mods(MOD_BIT_LSHIFT);
+                        layer_off(4);
+                        unregister_mods(MOD_HYPR);
+                    }
+                }
+                return false; // Skip default handling if it's a tap/tap+hold
+            }
+            return true; // Continue default handling.
+        default:
+            return true;
     }
     return true;
 }
@@ -118,8 +159,8 @@ const uint16_t PROGMEM keymaps[DYNAMIC_KEYMAP_LAYER_COUNT][MATRIX_ROWS][MATRIX_C
         /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL, XXXXXXX,
 
         /*Down                  Inner (pad)     Upper (Mode)    O.Upper (nail)  OL (knuckle) Pushthrough*/
-        /*RT*/ MO(NAS),         SPACE_HYPR_L5,       TO(FUNC),       KC_BSPC,        KC_LALT,     TG(NAS),
-        /*LT*/ KC_LSFT,         KC_ENTER,       TO(NORMAL),          KC_TAB,         KC_LCTL,     KC_CAPS
+        /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPC,        KC_LALT,     TG(NAS),
+        /*LT*/ KC_LSFT,         CTL_SFT,       TO(NORMAL),          KC_TAB,         KC_LCTL,     KC_CAPS
         ),
 
     [NORMAL_HOLD] = LAYOUT(
